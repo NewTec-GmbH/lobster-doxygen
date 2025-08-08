73f0ad270cdb802c73146d326360d9c4eaedb417
Knoll Dominik <Dominik.Knoll@newtec.de>
2025-08-08 14:39:07 +0200

    Merged level test projects to one project.

---
+++ tests/utils/cpp-level-test/src/FunctionPrototype.h
@@ -0,0 +1,43 @@
+/***************************************************************************************************
+  (c) NewTec GmbH 2025   -   www.newtec.de
+***************************************************************************************************/
+/**
+ @ingroup main_group 
+ @defgroup function_prototype_group FunctionPrototype module 
+
+ @brief Function prototype module 
+
+ Module with functions and prototypes.
+
+ @file FunctionPrototype.h
+***************************************************************************************************/
+#ifndef FUNCTION_PROTOTYPE_H
+#define FUNCTION_PROTOTYPE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* INCLUDES ***************************************************************************************/
+
+/* CONSTANTS **************************************************************************************/
+
+/* MACROS *****************************************************************************************/
+
+/* TYPES ******************************************************************************************/
+
+/* PROTOTYPES *************************************************************************************/
+
+/**
+ * This function is for requirement in prototype. 
+ * 
+ * @implements{SwRequirements.sw_req_prototype}
+ */
+extern void FunctionPrototype_foo(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* FUNCTION_PROTOTYPE_H */
