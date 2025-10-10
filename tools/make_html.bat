1bd5fa1015608e8742c74b1adfcc60e55d7944dd
Andreas Merkle <andreas.merkle@newtec.de>
2025-10-10 09:16:50 +0200

    All licenses adapted to GPL-v3 #39

--- tools/make_html.bat
+++ tools/make_html.bat
@@ -1,32 +1,19 @@
 @ECHO OFF
-REM BSD 3-Clause License
+REM lobster-doxygen - Doxygen XML to LOBSTER common interchange format converter
+REM Copyright (c) NewTec GmbH 2025   -   www.newtec.de
 REM
-REM Copyright (c) 2025, NewTec GmbH
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation, either version 3 of the License, or
+REM (at your option) any later version.
 REM
-REM Redistribution and use in source and binary forms, with or without
-REM modification, are permitted provided that the following conditions are met:
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
 REM
-REM 1. Redistributions of source code must retain the above copyright notice, this
-REM    list of conditions and the following disclaimer.
-REM
-REM 2. Redistributions in binary form must reproduce the above copyright notice,
-REM    this list of conditions and the following disclaimer in the documentation
-REM    and/or other materials provided with the distribution.
-REM
-REM 3. Neither the name of the copyright holder nor the names of its
-REM    contributors may be used to endorse or promote products derived from
-REM    this software without specific prior written permission.
-REM
-REM THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-REM AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-REM IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-REM DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-REM FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-REM DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-REM SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-REM CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-REM OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-REM OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+REM You should have received a copy of the GNU General Public License
+REM along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 setlocal
 
