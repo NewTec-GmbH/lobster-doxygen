d71264805e895fdb3224b205644fa989617aa415
Andreas Merkle <andreas.merkle@newtec.de>
2026-04-29 14:15:15 +0200

    Copyright 2026 updated

--- tests/test_general.py
+++ tests/test_general.py
@@ -4,7 +4,7 @@ Author: Dominik Knoll (dominik.knoll@newtec.de)
 """
 
 # lobster-doxygen - Doxygen XML to LOBSTER common interchange format converter
-# Copyright (c) NewTec GmbH 2025   -   www.newtec.de
+# Copyright (c) NewTec GmbH 2025 - 2026   -   www.newtec.de
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
